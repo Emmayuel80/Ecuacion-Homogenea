1ea5b93dc6d4d4c0af3f8aa0cb2257506a1c8279
Emmayuel80 <emmayuel80@gmail.com>
2018-05-14 00:44:45 -0500

    Add files via upload

---
+++ JS/ecuacion_general.js
@@ -0,0 +1,69 @@
+﻿// JavaScript source code
+var numero = new Object();
+var numero2 = new Object();
+numero.real = 0;
+numero.imaginario = 0;
+
+
+function calcEcuacionGeneral(a,b,c) {
+    var discri;
+    if (b == 0) {
+        if (c < 0) {
+            c = c * -1;
+        }
+        if (Math.sqrt(c) % 1 != 0) {
+            x1 = "√" + c;
+            x2 = "√" + c;
+        }
+        else {
+            x1 = Math.sqrt(c);
+            x2 = Math.sqrt(c);
+        }
+    }
+    else if (c == 0) {
+        x1 = 0;
+        x2 = b * -1;
+    }
+    else {
+        discri = b * b - 4 * a * c;
+
+        if (discri < 0) {
+            discri = discri * -1;
+
+
+            if (Math.sqrt(discri) % 1 != 0) {
+                numero.imaginario = "√" + (Math.sqrt(discri)).toFixed(0) + "/" + (2 * a);
+                numero2.imaginario = "√" + (Math.sqrt(discri)).toFixed(0) + "/" + (2 * a);
+            }
+            else {
+                numero.imaginario = Math.sqrt(discri) / (2 * a);
+                numero2.imaginario = Math.sqrt(discri) / (2 * a);
+            }
+            numero.real = (-b / (2 * a));
+            numero2.real = (-b / (2 * a));
+            x1 = numero.real + "+" + numero.imaginario+"i";
+
+            x2 = numero2.real +"+" + numero2.imaginario+"i";
+
+
+
+        }
+
+        else if (discri == 0) {
+
+            x1 = -b / (2 * a);
+            x2 = x1;
+
+
+        }
+
+        else if (discri > 0) {
+
+            x1 = (-b + Math.sqrt(discri)) / (2 * a);
+
+            x2 = (-b - Math.sqrt(discri)) / (2 * a);
+
+
+        }
+    }
+}
---
+++ JS/main.js
@@ -0,0 +1,70 @@
+﻿// JavaScript source code
+var a, b, c;
+function getData() {
+    a = document.getElementById("a").value;
+    b = document.getElementById("b").value;
+    c = document.getElementById("c").value;
+    a = Number(a);
+    b = Number(b);
+    c = Number(c);
+    procedimiento();
+    cambiarTab();
+    
+}
+function cambiarTab() {
+    document.getElementById("tab1").className = "mdl-layout__tab";
+    document.getElementById("fixed-tab-1").className = "mdl-layout__tab-panel";
+    document.getElementById("tab2").className = "mdl-layout__tab is-active";
+    document.getElementById("fixed-tab-2").className = "mdl-layout__tab-panel is-active";
+}
+function procedimiento() {
+    calcEcuacionGeneral(a, b, c);
+    calcEc_Homo();
+    calcLi();
+    calcEc_Carac();
+    getRaices();
+}
+function calcEc_Homo() {
+        document.getElementById("ec_homo").innerHTML = a+"y''+("+ b + "y')+(" + c + "y)=0";
+  
+}
+
+function calcEc_Carac() {
+    document.getElementById("ec_carac").innerHTML = a + "m^2+(" + b + "m)+(" + c + ")=0";
+}
+
+function getRaices() {
+    document.getElementById("raices").innerHTML = "Las raices son: m1=" + x1 + ", m2=" + x2;
+}
+
+
+function calcLi() {
+    if (numero.imaginario == 0 && b == 0 && c > 0) {
+        document.getElementById("yc").innerHTML = "yc= C1 " + " cos " + x1 + "x +" + "C2 " + " sen " + x2 + "x";
+    }
+    else if (numero.imaginario == 0 && b == 0 && c < 0) {
+        document.getElementById("yc").innerHTML = "yc= C1 e^" + x1 + "x +" + "C2 e^" + x2 + "x";
+    }
+    else if (numero.imaginario == 0 && c == 0) {
+        document.getElementById("yc").innerHTML = "yc= C1 " + "+" + " C2 e^" + x2 + "x";
+    }
+    else if (numero.imaginario == 0) {
+        if (x1 == x2) {
+            document.getElementById("yc").innerHTML = "yc= C1 e^" + x1 + "x +" + "C2 xe^" + x2 + "x";
+        }
+        else if (x1 != x2) {
+            document.getElementById("yc").innerHTML = "yc= C1 e^" + x1 + "x +" + "C2 e^" + x2 + "x";
+        }
+    }
+
+    else {
+        if (numero.real != 0) {
+            document.getElementById("yc").innerHTML = "yc= C1 e^" + numero.real + "x cos " + numero.imaginario + "x +" + "C2 e^" + numero2.real + "x sen " + numero2.imaginario + "x";
+        }
+        else {
+            document.getElementById("yc").innerHTML = "yc= C1 "+ " cos " + numero.imaginario + "x +" + "C2 " + " sen " + numero2.imaginario + "x";
+        }
+    }
+
+}
+
